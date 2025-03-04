983985809d92e8b815e1ef2d4a6da46f94bb4e2b
Michael von Domaros <mvondomaros@uni-marburg.de>
2025-03-04 16:17:28 +0100

    Add nvim config.

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,21 @@
+local map = vim.keymap.set
+
+-- Go to window
+map("n", "<C-Left>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<C-Down>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<C-Up>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<C-Right>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+-- Resize window
+map("n", "<S-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<S-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<S-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<S-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- Move Lines
+map("n", "<A-Down>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
+map("n", "<A-Up>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+map("i", "<A-Down>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map("i", "<A-Up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map("v", "<A-Down>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+map("v", "<A-Up>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,10 @@
+-- Spelling
+vim.opt.spell = true
+
+-- Indentation
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+
+--Python
+vim.g.lazyvim_python_ruff = "ruff"
