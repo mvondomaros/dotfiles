54c1fa7ab36cedc122273530c71970a2db6169fb
Michael von Domaros <mvondomaros@dawn.local>
2025-03-08 20:11:57 +0100

    Tweak homebrew bootstrap.

--- fish/conf.d/10_brew.fish
+++ fish/conf.d/10_brew.fish
@@ -1,8 +1,9 @@
 if set -q BREW_PREFIX
-    $BREW_PREFIX/bin/brew shellenv | source
+else if test -d /opt/homebrew
+    set -Ux BREW_PREFIX /opt/homebrew
 else if status is-interactive; and test (uname -s) = Darwin
     echo "Bootstrapping Homebrew"
     set -Ux BREW_PREFIX /opt/homebrew
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    $BREW_PREFIX/bin/brew shellenv | source
 end
+$BREW_PREFIX/bin/brew shellenv | source
