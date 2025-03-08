f744260a42a97ffe894d75b16b7c04904f3a82ae
Michael von Domaros <mvondomaros@dawn.local>
2025-03-08 21:13:42 +0100

    Tweak fish config.

--- fish/conf.d/20_env.fish
+++ fish/conf.d/20_env.fish
@@ -5,8 +5,6 @@ set -q LC_COLLATE; or set -Ux LC_COLLATE C
 # Set eza config dir.
 set -q EZA_CONFIG_DIR; or set -Ux EZA_CONFIG_DIR $XDG_CONFIG_HOME/eza
 
-if status is-interactive
-    # Set pager and browser.
-    set -q PAGER; or set -Ux PAGER less
-    set -q BROWSER; or set -Ux BROWSER open
-end
+# Set pager and browser.
+set -q PAGER; or set -Ux PAGER less
+set -q BROWSER; or set -Ux BROWSER open
