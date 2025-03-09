50639757db868d675dce57aa3a5ece9348741904
Michael von Domaros <mvondomaros@dawn.local>
2025-03-09 14:26:20 +0100

    Add paths.

--- fish/conf.d/00_init.fish
+++ fish/conf.d/00_init.fish
@@ -3,3 +3,6 @@ set -q XDG_CONFIG_HOME; or set -Ux XDG_CONFIG_HOME $HOME/.config
 set -q XDG_DATA_HOME; or set -Ux XDG_DATA_HOME $HOME/.local/share
 set -q XDG_STATE_HOME; or set -Ux XDG_STATE_HOME $HOME/.local/state
 set -q XDG_CACHE_HOME; or set -Ux XDG_CACHE_HOME $HOME/.cache
+
+# Set paths.
+fish_add_path ~/.local/bin
