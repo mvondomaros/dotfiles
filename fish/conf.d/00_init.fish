f39708ac1528f0666cae139e30078d7fd3b40df9
Michael von Domaros <mvondomaros@ma15135.Chemie.Uni-Marburg.DE>
2025-07-09 17:25:36 +0200

    Switch to pixi.

--- fish/conf.d/00_init.fish
+++ fish/conf.d/00_init.fish
@@ -4,5 +4,8 @@ set -q XDG_DATA_HOME; or set -Ux XDG_DATA_HOME $HOME/.local/share
 set -q XDG_STATE_HOME; or set -Ux XDG_STATE_HOME $HOME/.local/state
 set -q XDG_CACHE_HOME; or set -Ux XDG_CACHE_HOME $HOME/.cache
 
+# Set GIT_CONFIG
+set -q GIT_CONFIG; or set -Ux GIT_CONFIG $HOME/.config/git/.gitconfig
+
 # Set paths.
 fish_add_path ~/.local/bin
