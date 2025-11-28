a36c3df03a651c422bbec46ea894ddd3c9717dfd
vonDomaros Michael Prof. Dr. <domaros@hnode02.localdomain>
2025-11-28 18:40:12 +0100

    Add atuin bash support.

--- bash/plugins/atuin.sh
+++ bash/plugins/atuin.sh
@@ -1,5 +1,4 @@
 # Enable atuin bash integration.
-[[ -d ~/.opt/atuin ]] && export PATH=~/.opt/atuin:$PATH
-if type atuin >/dev/null 2>&1; then
+if command -v atuin >/dev/null 2>&1; then
     eval "$(atuin init bash)"
 fi
